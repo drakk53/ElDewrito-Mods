002abc1e1294d778b406ef623202e7123086f300
2174497765446E744D <96374300+2174497765446E744D@users.noreply.github.com>
2022-03-12 12:42:00 +1100

    Remove Duped Tags

---
+++ Main Menu Cache Script/mainmenucache/TT1/main_menu_cache_script_1.cmd
@@ -0,0 +1,40 @@
+nametag 0x27AE globals\global_shield_impact_settings
+deletetag 0x445B \\shaders\default_bitmaps\bitmaps\gray_50_percent.bitmap
+deletetag 0x446B \\shaders\default_bitmaps\bitmaps\color_white.bitmap
+deletetag 0x4465 \\shaders\default_bitmaps\bitmaps\reference_grids.bitmap
+deletetag 0x4483 \\shaders\default_bitmaps\bitmaps\default_dynamic_cube_map.bitmap
+deletetag 0x4486 \\shaders\default_bitmaps\bitmaps\color_red.bitmap
+deletetag 0x448A \\shaders\default_bitmaps\bitmaps\alpha_white.bitmap
+deletetag 0x4493 \\shaders\default_bitmaps\bitmaps\gray_50_percent_linear.bitmap
+deletetag 0x445A \\shaders\shader_options\albedo_default.render_method_option
+deletetag 0x4475 \\shaders\shader_options\alpha_test_off.render_method_option
+deletetag 0x4476 \\shaders\shader_options\alpha_test_on.render_method_option
+deletetag 0x06AB \\fx\particles\flare\explosive_flash.particle
+deletetag 0x18E3 \\fx\particles\flare\explosive_flash.particle
+deletetag 0x2186 \\fx\particles\flare\explosive_flash.particle
+deletetag 0x2229 \\fx\particles\flare\explosive_flash.particle
+deletetag 0x18E5 \\fx\particles\energy\spark_long.particle
+deletetag 0x25E2 \\fx\particles\energy\spark_long.particle
+deletetag 0x02D0 \\sound\dsp_effects\radio_static\radio_static_in.sound_looping
+deletetag 0x02D1 \\sound\dsp_effects\radio_static\radio_static_in.sound_looping
+deletetag 0x02E0 \\sound\lsnd\dtemple_underwater2.sound
+deletetag 0x5ADB \\multiplayer\game_variant_settings\territories\territories_respawn_on_capture.text_value_pair_definition
+deletetag 0x0811 \\ui\halox\start_menu\animations\sidebar\sidebar_unload1.gui_widget_position_animation_definition
+deletetag 0x0897 \\ui\halox\start_menu\panes\settings\settings_ui.bitmap
+deletetag 0x0981 \\ui\halox\start_menu\panes\settings_camera\sidebar_items.gui_datasource_definition
+deletetag 0x0A43 \\ui\halox\common\roster\animations\middle_ring.gui_widget_rotation_animation_definition
+deletetag 0x0A2C \\ui\halox\pregame_lobby\lobby_list_matchmaking.gui_datasource_definition
+deletetag 0x0A93 \\ui\halox\postgame_lobby\constants.gui_datasource_definition
+deletetag 0x4458 \\shaders\shader.render_method_definition
+deletetag 0x44DE \\shaders\terrain.render_method_definition
+deletetag 0x457A \\shaders\foliage.render_method_definition
+deletetag 0x466B \\shaders\decal.render_method_definition
+deletetag 0x4740 \\shaders\particle.render_method_definition
+deletetag 0x4753 \\shaders\beam.render_method_definition
+deletetag 0x4762 \\shaders\light_volume.render_method_definition
+deletetag 0x5013 \\shaders\water.render_method_definition
+deletetag 0x50CC \\shaders\halogram.render_method_definition
+savetagnames
+cleancachefiles
+rebuildcachefiles "../MM"
+quit
