dad3ef4e7c8a072ebf8fef6b3e7a9203481be38b
2174497765446E744D <96374300+2174497765446E744D@users.noreply.github.com>
2022-04-20 22:37:43 +1000

    Update main_menu_cache_script_4_CEAMP.cmd

--- mainmenucache/TT2/main_menu_cache_script_4_CEAMP.cmd
+++ mainmenucache/TT2/main_menu_cache_script_4_CEAMP.cmd
@@ -1,3 +1,175 @@
+opencachefile "../H3MCCMaps/bloodgulch.map"
+porttag ui\chud\animations\backpack_test.chud_animation_definition
+porttag ui\chud\animations\backpack_unready.chud_animation_definition
+porttag ui\chud\animations\backpack_warning_flash.chud_animation_definition
+porttag ui\chud\animations\beam_rifle_meter_left.chud_animation_definition
+porttag ui\chud\animations\beam_rifle_meter_right.chud_animation_definition
+porttag ui\chud\animations\carbine_crosshair.chud_animation_definition
+porttag ui\chud\animations\carbine_crosshair2.chud_animation_definition
+porttag ui\chud\animations\center_dimming.chud_animation_definition
+porttag ui\chud\animations\center_hack.chud_animation_definition
+porttag ui\chud\animations\clockwise.chud_animation_definition
+porttag ui\chud\animations\counterclockwise.chud_animation_definition
+porttag ui\chud\animations\ctf_scoreboard.chud_animation_definition
+porttag ui\chud\animations\dimming.chud_animation_definition
+porttag ui\chud\animations\equipment_kablam.chud_animation_definition
+porttag ui\chud\animations\fade_in.chud_animation_definition
+porttag ui\chud\animations\fade_in_slow.chud_animation_definition
+porttag ui\chud\animations\flash_test.chud_animation_definition
+porttag ui\chud\animations\init_wipe_horizontal.chud_animation_definition
+porttag ui\chud\animations\init_wipe_vertical.chud_animation_definition
+porttag ui\chud\animations\laser_charge.chud_animation_definition
+porttag ui\chud\animations\laser_unzoom_level.chud_animation_definition
+porttag ui\chud\animations\laser_zoom_fade_level.chud_animation_definition
+porttag ui\chud\animations\laser_zoom_level.chud_animation_definition
+porttag ui\chud\animations\lock_flash.chud_animation_definition
+porttag ui\chud\animations\motion_tracker_ping.chud_animation_definition
+porttag ui\chud\animations\pitch_slide.chud_animation_definition
+porttag ui\chud\animations\shield_dimming.chud_animation_definition
+porttag ui\chud\animations\shields_out.chud_animation_definition
+porttag ui\chud\animations\sniper_slide.chud_animation_definition
+porttag ui\chud\animations\vip_aura_center.chud_animation_definition
+porttag ui\chud\animations\vip_aura_pulsate.chud_animation_definition
+porttag ui\chud\animations\vip_notify.chud_animation_definition
+porttag ui\chud\animations\vip_notify_text.chud_animation_definition
+porttag ui\chud\animations\vox1.chud_animation_definition
+porttag ui\chud\animations\vox2.chud_animation_definition
+porttag ui\chud\animations\yaw_slide.chud_animation_definition
+porttag ui\chud\animations\zoom_scope.chud_animation_definition
+exit
+opencachefile "../H3MCCMaps/sidewinder_ce.map"
+porttag kashiiera\ui\chud\assault_rifle_ce
+porttag kashiiera\ui\chud\combatevolved\e_healthpack
+porttag kashiiera\ui\chud\combatevolved\needler_ce
+porttag kashiiera\ui\chud\combatevolved\plasma_pistol_ce
+porttag kashiiera\ui\chud\combatevolved\plasma_rifle_ce
+porttag kashiiera\ui\chud\combatevolved\rocket_launcher_ce
+porttag kashiiera\ui\chud\combatevolved\shotgun_ce
+porttag kashiiera\ui\chud\combatevolved\sniper_rifle_ce
+porttag kashiiera\ui\chud\flamethrower
+porttag kashiiera\ui\chud\fuel_rod_h1
+porttag kashiiera\ui\chud\gravity_rifle
+porttag kashiiera\ui\chud\magnum_ce
+porttag kashiiera\ui\chud\spartan_ce
+porttag ui\chud\monitor
+porttag ui\chud\sword
+porttag ui\chud\unarmed
+porttag ui\chud\v_banshee
+porttag ui\chud\v_chaingun
+porttag ui\chud\v_driver
+porttag ui\chud\v_ghost
+porttag ui\chud\v_scorpion
+porttag ui\chud\v_shade
+exit
+opencachefile "../H3MCCMaps/bloodgulch.map"
+porttag kashiiera\objects\characters\elite_ce\fp\fp.mode
+porttag kashiiera\objects\characters\elite_ce\fp_body\fp_body.mode
+porttag kashiiera\objects\characters\elite_ce\elite_ce.bipd
+porttag kashiiera\objects\characters\masterchief_ce\fp_body\fp_body.mode
+porttag kashiiera\objects\characters\masterchief_ce\fp\fp.mode
+porttag kashiiera\objects\characters\masterchief_ce\masterchief_ce_mp\masterchief_ce_mp.bipd
+porttag kashiiera\objects\characters\monitor_ce\monitor_ce.bipd
+exit
+opencachefile "../H3MCCMaps/bloodgulch.map"
+porttag objects\ui\editor_gizmo\editor_gizmo.scen
+porttag objects\multi\vip\vip_boundary.bloc 
+porttag kashiiera\objects\weapons\grenade\frag_grenade_ce\frag_grenade_ce.proj
+porttag kashiiera\objects\weapons\grenade\frag_grenade_ce\frag_grenade_ce.eqip
+porttag kashiiera\objects\weapons\grenade\plasma_grenade_ce\plasma_grenade_ce.proj
+porttag kashiiera\objects\weapons\grenade\plasma_grenade_ce\plasma_grenade_ce.eqip
+porttag kashiiera\objects\weapons\rifle\gravity_rifle\gravity_rifle.weap
+porttag kashiiera\objects\weapons\rifle\assault_rifle_ce\assault_rifle_ce.weap
+porttag kashiiera\objects\weapons\pistol\plasma_pistol_ce\plasma_pistol_ce.weap
+porttag kashiiera\objects\weapons\melee\forge_ball_ce\forge_ball_ce.weap
+porttag kashiiera\objects\weapons\multiplayer\flag_ce\flag_ce.weap
+porttag kashiiera\objects\weapons\melee\energy_sword_ce\energy_sword_ce.weap
+porttag kashiiera\objects\weapons\pistol\magnum_ce\magnum_ce.weap
+porttag kashiiera\objects\weapons\pistol\needler_ce\needler_ce.weap
+porttag kashiiera\objects\weapons\rifle\plasma_rifle_ce\plasma_rifle_ce.weap
+porttag kashiiera\objects\weapons\support_high\rocket_launcher_ce\rocket_launcher_ce.weap
+porttag kashiiera\objects\weapons\rifle\shotgun_ce\shotgun_ce.weap
+porttag kashiiera\objects\weapons\rifle\sniper_rifle_ce\sniper_rifle_ce.weap
+porttag kashiiera\objects\weapons\support_low\flamethrower_ce\flamethrower_ce.weap
+porttag kashiiera\objects\weapons\support_high\fuel_rod_h1\fuel_rod_h1.weap
+porttag objects\weapons\multiplayer\ball\ball.weap
+exit
+edittag globals\globals.globals
+setfield grenades[0].equipment kashiiera\objects\weapons\grenade\frag_grenade_ce\frag_grenade_ce.eqip
+setfield grenades[0].projectile kashiiera\objects\weapons\grenade\frag_grenade_ce\frag_grenade_ce.proj
+setfield grenades[1].equipment kashiiera\objects\weapons\grenade\plasma_grenade_ce\plasma_grenade_ce.eqip
+setfield grenades[1].projectile kashiiera\objects\weapons\grenade\plasma_grenade_ce\plasma_grenade_ce.proj
+setfield grenades[2].equipment kashiiera\objects\weapons\grenade\frag_grenade_ce\frag_grenade_ce.eqip
+setfield grenades[2].projectile kashiiera\objects\weapons\grenade\frag_grenade_ce\frag_grenade_ce.proj
+setfield grenades[3].equipment kashiiera\objects\weapons\grenade\plasma_grenade_ce\plasma_grenade_ce.eqip
+setfield grenades[3].projectile kashiiera\objects\weapons\grenade\plasma_grenade_ce\plasma_grenade_ce.proj
+setfield interfacetags[0].singleplayeruiglobals null
+setfield playerrepresentation[0].firstpersonhands kashiiera\objects\characters\masterchief_ce\fp_body\fp_body.mode
+setfield playerrepresentation[0].firstpersonbody kashiiera\objects\characters\masterchief_ce\fp\fp.mode
+setfield playerrepresentation[0].thirdpersonunit kashiiera\objects\characters\masterchief_ce\masterchief_ce_mp\masterchief_ce_mp.bipd
+setfield playerrepresentation[0].binocularszoominsound sound\game_sfx\ui\binoculars\binocs_in_click.snd!
+setfield playerrepresentation[0].binocularszoomoutsound sound\game_sfx\ui\binoculars\binocs_out_click.snd!
+setfield playerrepresentation[1].firstpersonhands kashiiera\objects\characters\elite_ce\fp\fp.mode
+setfield playerrepresentation[1].firstpersonbody kashiiera\objects\characters\elite_ce\fp_body\fp_body.mode
+setfield playerrepresentation[1].thirdpersonunit kashiiera\objects\characters\elite_ce\elite_ce.bipd
+setfield playerrepresentation[1].binocularszoominsound sound\game_sfx\ui\binoculars\binocs_in_click.snd!
+setfield playerrepresentation[1].binocularszoomoutsound sound\game_sfx\ui\binoculars\binocs_out_click.snd!
+setfield playerrepresentation[2].firstpersonhands kashiiera\objects\characters\masterchief_ce\fp_body\fp_body.mode
+setfield playerrepresentation[2].firstpersonbody kashiiera\objects\characters\masterchief_ce\fp\fp.mode
+setfield playerrepresentation[2].thirdpersonunit kashiiera\objects\characters\masterchief_ce\masterchief_ce_mp\masterchief_ce_mp.bipd
+setfield playerrepresentation[2].binocularszoominsound sound\game_sfx\ui\binoculars\binocs_in_click.snd!
+setfield playerrepresentation[2].binocularszoomoutsound sound\game_sfx\ui\binoculars\binocs_out_click.snd!
+setfield playerrepresentation[3].firstpersonhands kashiiera\objects\characters\elite_ce\fp\fp.mode
+setfield playerrepresentation[3].firstpersonbody kashiiera\objects\characters\elite_ce\fp_body\fp_body.mode
+setfield playerrepresentation[3].thirdpersonunit kashiiera\objects\characters\elite_ce\elite_ce.bipd
+setfield playerrepresentation[3].binocularszoominsound sound\game_sfx\ui\binoculars\binocs_in_click.snd!
+setfield playerrepresentation[3].binocularszoomoutsound sound\game_sfx\ui\binoculars\binocs_out_click.snd!
+setfield playerrepresentation[4].firstpersonhands kashiiera\objects\characters\elite_ce\fp\fp.mode
+setfield playerrepresentation[4].firstpersonbody kashiiera\objects\characters\elite_ce\fp_body\fp_body.mode
+setfield playerrepresentation[4].thirdpersonunit kashiiera\objects\characters\elite_ce\elite_ce.bipd
+setfield playerrepresentation[4].binocularszoominsound sound\game_sfx\ui\binoculars\binocs_in_click.snd!
+setfield playerrepresentation[4].binocularszoomoutsound sound\game_sfx\ui\binoculars\binocs_out_click.snd!
+setfield playerrepresentation[5].thirdpersonunit kashiiera\objects\characters\monitor_ce\monitor_ce.bipd
+savetagchanges
+exit
+edittag multiplayer\multiplayer_globals.multiplayer_globals
+setfield universal[0].gamevariantweapons[0].weapon kashiiera\objects\weapons\rifle\gravity_rifle\gravity_rifle.weap
+setfield universal[0].gamevariantweapons[1].weapon kashiiera\objects\weapons\rifle\assault_rifle_ce\assault_rifle_ce.weap
+setfield universal[0].gamevariantweapons[2].weapon kashiiera\objects\weapons\pistol\plasma_pistol_ce\plasma_pistol_ce.weap
+setfield universal[0].gamevariantweapons[3].weapon kashiiera\objects\weapons\melee\forge_ball_ce\forge_ball_ce.weap
+setfield universal[0].gamevariantweapons[4].weapon kashiiera\objects\weapons\multiplayer\flag_ce\flag_ce.weap
+setfield universal[0].gamevariantweapons[5].weapon null
+setfield universal[0].gamevariantweapons[6].weapon kashiiera\objects\weapons\melee\energy_sword_ce\energy_sword_ce.weap
+setfield universal[0].gamevariantweapons[7].weapon kashiiera\objects\weapons\pistol\magnum_ce\magnum_ce.weap
+setfield universal[0].gamevariantweapons[8].weapon kashiiera\objects\weapons\pistol\needler_ce\needler_ce.weap
+setfield universal[0].gamevariantweapons[9].weapon kashiiera\objects\weapons\rifle\plasma_rifle_ce\plasma_rifle_ce.weap
+setfield universal[0].gamevariantweapons[10].weapon kashiiera\objects\weapons\support_high\rocket_launcher_ce\rocket_launcher_ce.weap
+setfield universal[0].gamevariantweapons[11].weapon kashiiera\objects\weapons\rifle\shotgun_ce\shotgun_ce.weap
+setfield universal[0].gamevariantweapons[12].weapon kashiiera\objects\weapons\rifle\sniper_rifle_ce\sniper_rifle_ce.weap
+setfield universal[0].gamevariantweapons[13].weapon null
+setfield universal[0].gamevariantweapons[14].weapon null
+setfield universal[0].gamevariantweapons[15].weapon null
+setfield universal[0].gamevariantweapons[16].weapon null
+setfield universal[0].gamevariantweapons[17].weapon null
+setfield universal[0].gamevariantweapons[18].weapon null
+setfield universal[0].gamevariantweapons[19].weapon null
+setfield universal[0].gamevariantweapons[20].weapon kashiiera\objects\weapons\support_low\flamethrower_ce\flamethrower_ce.weap
+setfield universal[0].gamevariantweapons[21].weapon null
+setfield universal[0].gamevariantweapons[22].weapon null
+setfield universal[0].gamevariantweapons[23].weapon kashiiera\objects\weapons\support_high\fuel_rod_h1\fuel_rod_h1.weap
+setfield universal[0].gamevariantweapons[24].weapon null
+setfield universal[0].gamevariantweapons[25].weapon null
+setfield universal[0].gamevariantweapons[26].weapon null
+setfield universal[0].gamevariantequipment[0].grenade kashiiera\objects\weapons\grenade\frag_grenade_ce\frag_grenade_ce.eqip.weap
+setfield universal[0].gamevariantequipment[1].grenade kashiiera\objects\weapons\grenade\plasma_grenade_ce\plasma_grenade_ce.eqip.weap
+setfield runtime[0].editorbiped kashiiera\objects\characters\monitor_ce\monitor_ce.bipd
+setfield runtime[0].editorhelperobject objects\ui\editor_gizmo\editor_gizmo.scen
+setfield runtime[0].flag kashiiera\objects\weapons\multiplayer\flag_ce\flag_ce.weap
+setfield runtime[0].ball objects\weapons\multiplayer\ball\ball.weap
+setfield runtime[0].bomb null
+setfield runtime[0].vipinfluencearea objects\multi\vip\vip_boundary.bloc 
+setfield runtime[0].unknown kashiiera\objects\weapons\grenade\plasma_grenade_ce\plasma_grenade_ce.proj
+savetagchanges
+exit
 createtag bitm levels\ui\placeholder\placeholder\gfxt
 edittag levels\ui\placeholder\placeholder\gfxt.bitmap
 importbitmap 0 "../DDS/unknown.dds"
@@ -146,9 +318,4 @@ opencachefile "../H3MCCMaps/mainmenu.map"
 porttag *.scnr
 exit
 updatemapfilesalt "../H3MCCMaps/info"
-edittag globals\globals.globals
-setfield playerrepresentation[0].thirdpersonunit objects\characters\masterchief\mp_masterchief\mp_masterchief.biped
-setfield playerrepresentation[2].thirdpersonunit objects\characters\masterchief\mp_masterchief\mp_masterchief.biped
-savetagchanges
-exit
 quit
